acd21f598f79f806dd15bb0d53fc40d5e7db470d
Apinya Pituxcoosuvarn (Air) <apinya.pituxcoosuvarn@exxonmobil.com>
2023-07-06 22:02:12 +0700

    Update table

--- script.sh
+++ script.sh
@@ -5,8 +5,8 @@ echo "" >> README.md
 echo "Fetched on $(date)" >> README.md
 echo "GitHub Sort Content: [LINK](https://github.com/Mottie/GitHub-userscripts/wiki/GitHub-sort-content)" >> README.md
 echo "" >> README.md
-echo "Brand | Name | Spec | Price | Sale Price" >> README.md
-echo "- | - | - | - | -" >> README.md
+echo "| Brand | Name | Spec | Price | Sale Price |" >> README.md
+echo "| - | - | - | - | - |" >> README.md
 
 # declare an array variable
 declare -a MENUS=(
@@ -24,5 +24,5 @@ for i in "${MENUS[@]}"
 do
   curl 'https://www.advice.co.th/avi/getProduct' \
     --data-raw "menu_level=2&menu=${i}&skip=0&take=100&key=0&sort_promotion=" |
-  jq -r '.res[] | "\(.item_brand_name) | [\(.item_name)](\(.item_url)) | \(.item_spec) | \(.item_srp|tonumber|round) | \(.item_saleprice|tonumber|round)"' >> README.md
+  jq -r '.res[] | "| \(.item_brand_name) | [\(.item_name)](\(.item_url)) | \(.item_spec) | \(.item_srp|tonumber|round) | \(.item_saleprice|tonumber|round) |"' >> README.md
 done
